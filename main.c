587de84e6c01a15f81eacd5d85095449c1220fc5
miblak <115421134+miblak@users.noreply.github.com>
2022-11-19 22:40:01 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 int ADC0_Read()						//channel ADC0
 {
 	ADMUX = 0b01000000;				//REFS0->1, ADLAR->0, ADC->0
-	ADCSRA |= (1<<ADSC);			//start conversion
-	while ((ADCSRA &(1<<ADIF))==0);	//checking if conversion has ended
+	ADCSRA |= (1<<ADSC);				//start conversion
+	while ((ADCSRA &(1<<ADIF))==0);			//checking if conversion has ended
 	ADCSRA |=(1<<ADIF);				//set ADIF bit of ADCSRA register
 	return(ADCW);					//return the ADCW
 }
@@ -28,16 +28,16 @@ int ADC2_Read()						//channel ADC2
 
 int main(void)
 {
-	DDRC=(0<<0)|(0<<1)|(0<<2);		//ADC0, ADC1, ADC2 ports as input
+	DDRC=(0<<0)|(0<<1)|(0<<2);			//ADC0, ADC1, ADC2 ports as input
 	DDRB=(1<<1);					//set PB1 as output
 	DDRB=(0<<7);					//set PB7 as input (for pushbutton)
 	ADCSRA = 0x87;					//enable ADC with given parameters
 	int ADC0val=0, ADC1val=0, ADC2val=0;
     while (1) 
     {
-		ADC0val=ADC0_Read();		//if the sensor does not detect moisture
-		ADC1val=ADC1_Read();		//it outputs the value 1023
-		ADC2val=ADC2_Read();		//(1024 samples in range from 0V to 5V)
+		ADC0val=ADC0_Read();			//if the sensor does not detect moisture
+		ADC1val=ADC1_Read();			//it outputs the value 1023
+		ADC2val=ADC2_Read();			//(1024 samples in range from 0V to 5V)
 		if((PINB & (1<<7)))
 		{
 			PORTB|=(1<<7);			//for DC motor
